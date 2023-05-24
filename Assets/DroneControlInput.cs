ac050db68f030581781331b1896b4b8189ed3f2f
Cym3ra <112971484+Cym3ra@users.noreply.github.com>
2023-05-24 15:26:39 +0200

    drone movement plus camera implemented

---
+++ Assets/DroneControlInput.cs
@@ -0,0 +1,127 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class DroneControlInput : MonoBehaviour
+{
+    private static DroneControlInput instance;
+    
+    public static DroneControlInput Instance
+    {
+        get { return instance; }
+    }
+
+    private DroneInput droneInput;
+
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            instance = this;
+        }
+
+        droneInput = new DroneInput();
+        Cursor.visible = false;
+    }
+
+    private void OnEnable()
+    {
+        droneInput.Enable();
+    }
+
+    private void OnDisable()
+    {
+        droneInput.Disable();
+    }
+
+    public Vector2 GetDroneMovement()
+    {
+        return droneInput.Drone.Move.ReadValue<Vector2>();
+    }
+
+    public Vector2 GetMouseDelta()
+    {
+        return droneInput.Drone.Look.ReadValue<Vector2>();
+    }
+
+    /*[Header("Movement")]
+        public float moveSpeed;
+        private Vector2 curMovementInput;
+
+        [Header("Look")]
+        public Transform cameraContainer;
+        public float minXLook;
+        public float maxXLook;
+        private float camCurXRot;
+        public float lookSensitivity;
+
+        private Vector2 mouseDelta;
+
+        [HideInInspector]
+        public bool canLook = true;
+
+        private Rigidbody rgbd;
+
+        private void Awake()
+        {
+            rgbd = GetComponent<Rigidbody>();
+        }
+
+        void Start()
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+
+        private void FixedUpdate()
+        {
+            Move();
+        }
+
+        private void LateUpdate()
+        {
+            if (canLook)
+            {
+                CameraLook();
+            }
+        }
+
+        private void Move()
+        {
+            Vector3 dir = transform.forward * curMovementInput.y + transform.right * curMovementInput.x;
+            dir *= moveSpeed;
+            dir.y = rgbd.velocity.y;
+
+            rgbd.velocity = dir;
+        }
+
+        private void CameraLook()
+        {
+            camCurXRot += mouseDelta.y * lookSensitivity;
+            camCurXRot = Mathf.Clamp(camCurXRot, minXLook, maxXLook);
+            cameraContainer.localEulerAngles = new Vector3(-camCurXRot, 0, 0);
+
+            transform.eulerAngles += new Vector3(0, mouseDelta.x * lookSensitivity, 0);
+        }
+
+        public void OnLookInput(InputAction.CallbackContext context)
+        {
+            mouseDelta = context.ReadValue<Vector2>();
+        }
+
+        public void OnMoveInput(InputAction.CallbackContext context)
+        {
+            if (context.phase == InputActionPhase.Performed)
+            {
+                curMovementInput = context.ReadValue<Vector2>();
+            }
+            else if (context.phase == InputActionPhase.Canceled)
+            {
+                curMovementInput = Vector2.zero;
+            }
+        }*/
+}
---
+++ Assets/Scripts/DroneController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DroneController : MonoBehaviour
+{
+
+    private CharacterController controller;
+    private Vector3 playerVelocity;
+    private DroneControlInput droneInput;
+    private float playerSpeed = 2.0f;
+    private Transform cameraTransform;
+    private float gravityValue = -9.81f;
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        droneInput = DroneControlInput.Instance;
+        cameraTransform = Camera.main.transform;
+    }
+
+    void Update()
+    {
+        if (playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+        }
+
+        Vector2 movement = droneInput.GetDroneMovement();
+        Vector3 move = new Vector3(movement.x, 0f, movement.y);
+        move = cameraTransform.forward * move.z + cameraTransform.right * move.x;
+        move.y = 0f;
+        controller.Move(move * Time.deltaTime * playerSpeed);
+
+        /*if (move != Vector3.zero)
+        {
+            gameObject.transform.forward = move;
+        }*/
+
+        playerVelocity.y += gravityValue * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
+    }
+}
